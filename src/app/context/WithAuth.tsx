8a36fd4d4b52ffd1316107726736e3731d85192a
mohadeev <94206815+mohadeev@users.noreply.github.com>
2024-08-14 00:07:44 +0200

    fix ing las time

--- src/app/context/WithAuth.tsx
+++ src/app/context/WithAuth.tsx
@@ -1,21 +1,26 @@
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, ComponentType } from 'react'
 
-const WithAuth = (WrappedComponent: any) => {
-	const { data: session, status } = useSession()
-	const router = useRouter()
-	useEffect(() => {
-		if (status === 'unauthenticated') {
-			router.push('/login')
-		}
-	}, [status, router])
-	return (props: any) => {
+function withAuth<T>(WrappedComponent: ComponentType<any>) {
+	const AuthenticatedComponent = (props: T) => {
+		const { data: session, status } = useSession()
+		const router = useRouter()
+
+		useEffect(() => {
+			if (status === 'unauthenticated') {
+				router.push('/login')
+			}
+		}, [status, router])
+
 		if (status === 'loading') {
 			return <div>Loading...</div>
 		}
+
 		return <WrappedComponent {...props} />
 	}
+
+	return AuthenticatedComponent
 }
 
-export default WithAuth
+export default withAuth
