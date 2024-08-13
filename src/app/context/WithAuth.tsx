8749691da14cc28930a5f27865d4a740d6ca2d25
mohadeev <94206815+mohadeev@users.noreply.github.com>
2024-08-13 23:57:47 +0200

    fix WithAuth

--- src/app/context/WithAuth.tsx
+++ src/app/context/WithAuth.tsx
@@ -3,16 +3,14 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 const WithAuth = (WrappedComponent: any) => {
+	const { data: session, status } = useSession()
+	const router = useRouter()
+	useEffect(() => {
+		if (status === 'unauthenticated') {
+			router.push('/login')
+		}
+	}, [status])
 	return (props: any) => {
-		const { data: session, status } = useSession()
-		const router = useRouter()
-
-		useEffect(() => {
-			if (status === 'unauthenticated') {
-				router.push('/login')
-			}
-		}, [status])
-
 		if (status === 'loading') {
 			return <div>Loading...</div>
 		}
