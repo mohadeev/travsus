be7ec10d1e7ce53986062d01b335ad2e74af58da
mohadeev <94206815+mohadeev@users.noreply.github.com>
2024-08-17 01:16:59 +0200

    all code

--- src/app/api/user/userData/route.ts
+++ src/app/api/user/userData/route.ts
@@ -2,7 +2,7 @@ import prisma from '@/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '../../auth/[...nextauth]/authOptions'
-
+//coment
 export async function GET(request: NextRequest) {
 	try {
 		const session: any = await getServerSession(authOptions)
@@ -12,14 +12,12 @@ export async function GET(request: NextRequest) {
 				{ status: 401 },
 			)
 		}
-
 		const user = await prisma.user.findUnique({
 			where: { email: session.user.email },
 		})
 		if (!user) {
 			return NextResponse.json({ message: 'User not found' }, { status: 404 })
 		}
-		console.log('user', user)
 		return NextResponse.json({ user: user, message: 'user_found' })
 	} catch (error) {
 		console.error('Error fetching user data:', error)
