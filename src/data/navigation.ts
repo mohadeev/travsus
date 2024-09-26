b47c05624c0846582549655ee97d73f0e51f9907
mohadeev <94206815+mohadeev@users.noreply.github.com>
2024-09-26 16:45:47 +0200

    DF

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -151,7 +151,7 @@ const templatesChildrenMenus: NavItemType[] = [
 		],
 	},
 	//
-	{ id: ncNanoId(), href: '/checkout', name: 'Checkout' },
+	// { id: ncNanoId(), href: '/checkout/12343', name: 'Checkout' },
 	{ id: ncNanoId(), href: '/pay-done', name: 'Pay done' },
 	//
 	{ id: ncNanoId(), href: '/author', name: 'Author page' },
