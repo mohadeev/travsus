82fab9d8da1b2b12eea8f5db9d31c7ee0caed755
mohadeev <skendoulmohamed@gmail.com>
2024-10-04 01:44:51 +0200

    DF

--- src/utils/email/emailTypes.ts
+++ src/utils/email/emailTypes.ts
@@ -20,7 +20,7 @@ const emailList = [
 		email: 'notify@travsus.com',
 		password: 'es2e*ghA',
 		// password: 'F1!dG3n7*zR@Pq5#',
-		subject: 'Welcome to your exclusive travel community!',
+		subject: 'Your Tripadvisor password',
 		sender: `"Travsus" <notify@travsus.com>`,
 		template: forgetPassword,
 	},
