0072d7a4a61239497380cc2c654d7184b15607e6
mohadeev <skendoulmohamed@gmail.com>
2024-10-14 23:29:56 +0200

    DF

---
+++ src/app/(dashboard)/dashboard/[pageName]/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import Breadcrumb from '@/components/Breadcrumbs/Breadcrumb'
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+
+// }
+
+const Settings = () => {
+	return (
+		<DefaultLayout>
+			<div className="mx-auto max-w-270"></div>
+		</DefaultLayout>
+	)
+}
+
+export default Settings
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,6 +86,7 @@ export default function RootLayout({
 	// const pathname = usePathname()
 	const isDashboardPath = pathname?.startsWith('/dashboard') ?? false
 	console.log('isDashboardPath: ', isDashboardPath)
+
 	return (
 		<html lang="en" className={inter.className}>
 			<head>
@@ -110,6 +111,23 @@ export default function RootLayout({
 						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WHQK9Z5M');`,
 					}}
 				/>
+
+				{!isLocalhost && (
+					<Script
+						id="hotjar-init"
+						strategy="afterInteractive"
+						dangerouslySetInnerHTML={{
+							__html: `(function(h,o,t,j,a,r){
+								h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+								h._hjSettings={hjid:5171033,hjsv:6};
+								a=o.getElementsByTagName('head')[0];
+								r=o.createElement('script');r.async=1;
+								r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+								a.appendChild(r);
+							})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+						}}
+					/>
+				)}
 			</head>
 			{/* Google Tag Manager (noscript) */}
 			<noscript>
