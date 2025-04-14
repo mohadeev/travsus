71e5518fc0c70dea34044c6187c62c7a68e2c0e3
mohadeev <skendoulmohamed@gmail.com>
2025-04-14 16:19:20 +0200

    save

--- src/app/(dashboard)/dashboard/tours/create/page.tsx
+++ src/app/(dashboard)/dashboard/tours/create/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-// import { DashboardShell } from '@/components/dashboard/shell'
 import { DashboardHeader } from '@/components/dashboard/header'
 import { Button } from '@/components/ui/button'
 import {
