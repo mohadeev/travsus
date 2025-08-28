924eeed35efed0664114bd6a89bbaef7ecbdbdb5
massindev <skendoulmohamed@gmail.com>
2025-08-28 12:32:16 +0200

    save renamed json files

--- src/components/TawkToWidget.tsx
+++ src/components/TawkToWidget.tsx
@@ -2,41 +2,12 @@
 
 import Script from 'next/script'
 import { useLocale } from 'next-intl'
-import { useEffect } from 'react'
 import tawkWidgets from '@/constants/tawkWidgets.json'
 
 export default function TawkToWidget() {
 	const locale = useLocale()
-
-	// normalize: convert "es-ES" -> "es"
-	const tawkLocale = locale || 'en-US'
-
-	useEffect(() => {
-		// if (typeof window !== 'undefined' && window.Tawk_API) {
-		// 	window.Tawk_API.onLoad = function () {
-		// 		window.Tawk_API.setLanguage(tawkLocale)
-		// 	}
-		// }
-
-		if (typeof window !== 'undefined' && window.Tawk_API) {
-			window.Tawk_API.onLoad = () => {
-				const locale = navigator.language.split('-')[0] // e.g. "es"
-				const messages: Record<string, string> = {
-					en: '👋 Hi! How can we help?',
-					es: '👋 ¡Hola! ¿En qué podemos ayudarte?',
-					de: '👋 Hallo! Wie können wir helfen?',
-				}
-
-				// Send a custom greeting dynamically
-				if (messages[locale]) {
-					window.Tawk_API.addEvent('custom_greeting', {
-						message: messages[locale],
-					})
-				}
-			}
-		}
-	}, [tawkLocale])
-
+	const tawkLocale = locale || 'en-us'
+	console.log(tawkWidgets['es-es'])
 	return (
 		<Script
 			id="tawk-to-widget"
