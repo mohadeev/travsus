d9c17e4ed9143ef5082afcef4138f13a00d537c0
mohadeev <skendoulmohamed@gmail.com>
2025-09-06 06:40:20 +0200

    sve

--- src/app/(client-components)/(HeroSearchFormSmall)/(experiences-search-form)/ExperiencesSearchForm.tsx
+++ src/app/(client-components)/(HeroSearchFormSmall)/(experiences-search-form)/ExperiencesSearchForm.tsx
@@ -5,34 +5,35 @@ import LocationInput from '../LocationInputDesktop'
 import GuestsInput from '../GuestsInputDesktop'
 import ExperiencesDateSingleInput from './ExperiencesDateSingleInput'
 import { Form, Field } from 'react-final-form'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 export interface ExperiencesSearchFormProps {}
 
 const ExperiencesSearchForm: FC<ExperiencesSearchFormProps> = ({}) => {
+	const searchParams = useSearchParams()
+	const pathname = usePathname()
+	const { replace } = useRouter()
+	// function handleSearch() {
+	// 	const params = new URLSearchParams(searchParams)
+
+	// 	replace(`/search?${params.toString()}`)
+	// }
+	// const subLink = handleSearch()
 	const renderForm = () => {
 		const onSubmit = () => {}
 		return (
-			<Form
-				onSubmit={onSubmit}
-				// validate={validate}
-				render={({ handleSubmit }) => (
-					<form onSubmit={handleSubmit}>
-						<form className="relative flex w-full flex-row rounded-full border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-neutral-800">
-							<LocationInput
-								// onInputDone={() => setDateFocused(true)}
-								className="flex-[1.5]"
-							/>
-							<div className="h-8 self-center border-r border-slate-200 dark:border-slate-700"></div>
-							<ExperiencesDateSingleInput className="flex-[1.2]" />
-							<div className="h-8 self-center border-r border-slate-200 dark:border-slate-700"></div>
-							<GuestsInput
-								className="flex-1"
-								// submitLink="/search?query=Marrakech"
-							/>
-						</form>
-					</form>
-				)}
-			/>
+			<form>
+				<form className="relative flex w-full flex-row rounded-full border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-neutral-800">
+					<LocationInput
+						// onInputDone={() => setDateFocused(true)}
+						className="flex-[1.5]"
+					/>
+					<div className="h-8 self-center border-r border-slate-200 dark:border-slate-700"></div>
+					<ExperiencesDateSingleInput className="flex-[1.2]" />
+					<div className="h-8 self-center border-r border-slate-200 dark:border-slate-700"></div>
+					<GuestsInput className="flex-1" />
+				</form>
+			</form>
 		)
 	}
 
